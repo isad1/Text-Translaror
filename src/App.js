b45185f4408f42a603226a23fe3bfde8b4bbb0c2
isad1 <ayushshrivastava06041997@gmail.com>
2021-11-23 22:29:15 +0530

    Text translation app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Translator from './Translator'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Translator/>
+        
+    </div>
+  );
+}
+
+export default App;
